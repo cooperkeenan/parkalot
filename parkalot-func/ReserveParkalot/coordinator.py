c6a8b1e3ac807c5a08abcc8e9c83bc90e5ce17a0
cooperkeenan <157698494+cooperkeenan@users.noreply.github.com>
2025-06-08 22:39:23 +0000

    seems to be working?

--- parkalot-func/ReserveParkalot/coordinator.py
+++ parkalot-func/ReserveParkalot/coordinator.py
@@ -59,14 +59,14 @@ def wait_for_reservation_time():
     if ACTIVE:
         now = datetime.utcnow()
         # Set to 11:00:01 UTC (12:00:01 UK BST)
-        target_time = now.replace(hour=21, minute=6, second=1, microsecond=0)
+        target_time = now.replace(hour=11, minute=0, second=2, microsecond=0)
         if target_time <= now:
             target_time += timedelta(days=1)
         wait_secs = (target_time - now).total_seconds()
         logging.info(f"Sleeping for {wait_secs:.0f}s until {target_time.time()} UTC (12:00:01 UK time)")
         time.sleep(wait_secs)
     else:
-        logging.info("ACTIVE=False: Skipping wait, running immediately for testing")VE=False: Skipping wait, running immediately for testing")
+        logging.info("ACTIVE=False: Skipping wait, running immediately for testing")
 
 
 # Reload the calendar page
